b15ca20665b9e37de8afc05101baf83104f7d612
mahitsy <david.mahitsy@gmail.com>
2018-03-07 23:31:34 +0200

    1-3 done

---
+++ 01. JavaScript Syntax, Basic Web (DOM, Events)/Lab/03. Sum Two Numbers.js
@@ -0,0 +1,9 @@
+function sumTwoNumbers(numbers) {
+
+    let a = Number(numbers[0])
+    let b = Number(numbers[1])
+
+    console.log(a + b)
+}
+
+sumTwoNumbers(['10', '20'])
---
+++ 01. JavaScript Syntax, Basic Web (DOM, Events)/Lab/04. Three Integers Sum.js
@@ -0,0 +1,5 @@
+function threeInt(a, b, c) {
+
+}
+
+threeInt(8, 15, 7)
